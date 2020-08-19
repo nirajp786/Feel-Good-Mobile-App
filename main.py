e7f4bb78fc0288031fb7a3e2555b86fca40e4cfd
Niraj Patel <786nirajpatel@gmail.com>
2020-08-19 19:16:38 -0400

    Made basic interface

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+    
+if __name__ == "__main__":
+    MainApp().run()
