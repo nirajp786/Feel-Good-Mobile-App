4487fa4d3182854ba8d32f24e3008a36d34b7332
Niraj Patel <786nirajpatel@gmail.com>
2020-08-25 21:21:18 -0400

    Implmented the sign up page :sunglasses:

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+import datetime
 
 Builder.load_file('design.kv')
 
+
+            
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+        
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+ 
+        self.manager.current = "sign_up_screen_success"
+        
+class SignUpScreenSuccess(Screen):
+    def success(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
 
 class RootWidget(ScreenManager):
     pass
